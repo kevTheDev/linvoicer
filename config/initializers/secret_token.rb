d45b55e72d9047582cc77e67109b65f26f1c3924
kevTheDev <kev.j.edwards@gmail.com>
2013-06-03 20:09:23 +0100

    Created new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Linvoicer::Application.config.secret_key_base = '4247ef801bf5b50356aacb5e3281255541bf5fe2fbf1670cc87a634807b2ee2d23d783744e079f6cea343d24a0ad4afb5bfad7a434ad971baa88028605df7be4'
