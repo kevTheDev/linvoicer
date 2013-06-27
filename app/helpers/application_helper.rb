a681807a534f54ffa8f9e8d27cfa894b9ccbc686
kevTheDev <kev.j.edwards@gmail.com>
2013-06-27 12:53:59 +0100

    Fixed link_to_add_fields

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,20 +5,16 @@ module ApplicationHelper
     (start_year - 2..start_year + 2).to_a
   end
   
-  def link_to_add_fields(name, f, association, parent_object)
-#    Rails.logger.info "Fobject: #{parent_object}"
-    #parent_object = Invoice.find(parent_object)
-    
-    new_object = parent_object.send(association).klass.new
-    id = new_object.object_id
-    
-    partial_name = "#{association.to_s.singularize}_fields"
-    
-    fields = f.fields_for(association, new_object, child_index: id) do |builder|
-      render(partial: partial_name, locals: {f: builder, parent_object: parent_object})
+  def link_to_remove_fields(name, f)
+    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
+  end
+
+  def link_to_add_fields(name, f, association)
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+      render(association.to_s.singularize + "_fields", :f => builder)
     end
-    
-    link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
+    link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
   end
   
   
