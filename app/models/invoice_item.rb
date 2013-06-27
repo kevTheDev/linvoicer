e205fcf41f77b8988156a710b72eb155911c7e5f
kevTheDev <kev.j.edwards@gmail.com>
2013-06-27 13:49:04 +0100

    Added InvoiceItem#time_in_hours

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -3,6 +3,17 @@ class InvoiceItem < ActiveRecord::Base
   belongs_to :client
   
   belongs_to :invoice
+
+  # http://stackoverflow.com/questions/14903379/rounding-to-nearest-fraction-half-quarter-etc  
+  
+  # returns the duration to the nearest quarter hour
+  def time_in_hours
+    return 0 if start == finish
+    
+    difference = (finish - start) / 1.hour
+    floor = (difference * 4).floor
+    floor / 4.0
+  end
   
 end
 
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -11,6 +11,55 @@ describe InvoiceItem do
   end
   # Associations
   
+  describe 'time_in_hours' do
+    
+    context 'zero time' do
+    
+      before do
+        start_and_end = Time.now
+        @invoice_item = build(:invoice_item, start: start_and_end, finish: start_and_end)
+      end
+      
+      it 'returns zero' do
+        @invoice_item.time_in_hours.should == 0
+      end
+    end
+    # zero_time
+    
+    context 'only whole hours worked' do
+      
+      before do
+        start  = Time.now
+        finish = 1.hour.from_now(start)
+        
+        @invoice_item = build(:invoice_item, start: start, finish: finish)
+      end
+      
+      it 'returns zero' do
+        @invoice_item.time_in_hours.should == 1
+      end
+      
+    end
+    
+    context 'fractional hours worked' do
+      
+      before do
+        start         = Time.now
+        hour_from_now = 1.hour.from_now
+        finish        = 15.minutes.from_now(hour_from_now)
+        
+        @invoice_item = build(:invoice_item, start: start, finish: finish)
+      end
+      
+      it 'returns zero' do
+        @invoice_item.time_in_hours.should == 1.25
+      end
+      
+      
+    end
+    
+  end
+  # time_in_hours
   
 end
 
